fe5d3f37ebb5ff45d0fca0155effc8f2df08703a
Wallysson <118409802+WallyssonSousa@users.noreply.github.com>
2025-10-20 21:23:37 -0300

    Feat: atualizando as rotas de transações

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -7,10 +7,10 @@ const router = Router();
 router.use(authMiddleware);
 
 router.post("/", TransactionController.create);
-router.get("/", TransactionController.list);
-router.get("/summary", TransactionController.summary);
-router.get("/:id", TransactionController.get);
 router.put("/:id", TransactionController.update);
 router.delete("/:id", TransactionController.delete);
+router.get("/", TransactionController.list);
+
+
 
 export default router;
